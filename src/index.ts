1a36b7c5f5a8ef69243095e2c05136f14792e20f
Henrique <henriquecastro@unifei.edu.br>
2022-07-26 10:02:02 -0300

    fix: index issues

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,29 @@
+import cors from "cors";
+import { config as dtoEnvConfig } from "dotenv";
+import express from "express";
+import helmet from "helmet";
+import { checkToken } from "./middlewares/security";
 import routes from "./routes/routes";
-import app from "./server";
+
+dtoEnvConfig();
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(helmet());
+app.use(cors());
+app.use(checkToken);
 
 app.use(routes);
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(process.env["PORT"], () => {
+  console.log("listening on port " + process.env["PORT"]);
+});
+
+export default app;
