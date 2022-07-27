84a1f75177c97995ccf8ed1cf4bbe109bf486f10
Henrique <henriquecastro@unifei.edu.br>
2022-07-27 11:24:58 -0300

    feat: getAllAssociados

--- src/controllers/associado.ts
+++ src/controllers/associado.ts
@@ -1,52 +1,63 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
 export enum AssociadoStatus {
-  Grad = "Grad",
-  Posgrad = "Posgrad",
-  Prof = "Prof",
+	Grad = 'Grad',
+	Posgrad = 'Posgrad',
+	Prof = 'Prof',
 }
 
 export interface ICreateAssociado {
-  nome: string;
-  email: string;
-  endereco: string;
-  status: AssociadoStatus;
+	nome: string;
+	email: string;
+	endereco: string;
+	status: AssociadoStatus;
 }
 
 const getTempoDevolucao = (status: AssociadoStatus) => {
-  switch (status) {
-    case AssociadoStatus.Grad:
-      return 7;
-    case AssociadoStatus.Posgrad:
-      return 10;
-    case AssociadoStatus.Prof:
-      return 14;
-  }
+	switch (status) {
+		case AssociadoStatus.Grad:
+			return 7;
+		case AssociadoStatus.Posgrad:
+			return 10;
+		case AssociadoStatus.Prof:
+			return 14;
+	}
+};
+
+const getAllAssociados = async () => {
+	const associados = await prisma.aSSOCIADO.findMany();
+
+	return {
+		status: 200,
+		message: 'Exemplares encontrados com sucesso',
+		data: associados,
+	};
 };
 
 const createAssociado = async (params: ICreateAssociado) => {
-  const { email, endereco, nome, status } = params;
-  const created = await prisma.aSSOCIADO.create({
-    data: {
-      Email: email,
-      Endereco: endereco,
-      Nome: nome,
-      Status: status,
-    },
-  });
-  if (!created) {
-    throw new Error("Erro ao criar associado");
-  }
+	const { email, endereco, nome, status } = params;
+	const created = await prisma.aSSOCIADO.create({
+		data: {
+			Email: email,
+			Endereco: endereco,
+			Nome: nome,
+			Status: status,
+		},
+	});
+	if (!created) {
+		throw new Error('Erro ao criar associado');
+	}
 
-  return {
-    status: 201,
-    message: "Associado criado com sucesso",
-  };
+	return {
+		status: 201,
+		message: 'Associado criado com sucesso',
+	};
 };
 
 export default {
-  createAssociado,
-  getTempoDevolucao,
+	createAssociado,
+	getTempoDevolucao,
+  getAllAssociados,
 };
--- src/routes/associado.ts
+++ src/routes/associado.ts
@@ -1,27 +1,41 @@
-import express, { Request, Response } from "express";
-import controllers from "../controllers/controllers";
+import express, { Request, Response } from 'express';
+import controllers from '../controllers/controllers';
 
 const router = express.Router();
 
-router.post("/", async (req: Request, res: Response) => {
-  try {
-    const { email, endereco, nome, status } = req.body;
+router.post('/', async (req: Request, res: Response) => {
+	try {
+		const { email, endereco, nome, status } = req.body;
 
-    const response = await controllers.associado.createAssociado({
-      email,
-      endereco,
-      nome,
-      status,
-    });
+		const response = await controllers.associado.createAssociado({
+			email,
+			endereco,
+			nome,
+			status,
+		});
 
-    return res.status(response.status).send(response);
-  } catch (err) {
-    console.log(err);
-    return res.status(500).send({
-      status: 500,
-      message: "Erro ao criar associado",
-    });
-  }
+		return res.status(response.status).send(response);
+	} catch (err) {
+		console.log(err);
+		return res.status(500).send({
+			status: 500,
+			message: 'Erro ao criar associado',
+		});
+	}
+});
+
+router.get('/', async (req: Request, res: Response) => {
+	try {
+		const response = await controllers.associado.getAllAssociados();
+
+		return res.status(response.status).send(response);
+	} catch (err) {
+		console.log(err);
+		return res.status(500).send({
+			status: 500,
+			message: 'Erro ao recuperar associados',
+		});
+	}
 });
 
 export default router;
